e20818deb7a7a7d30159f5e39fabd5af872c18fb
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-04-04 12:49:43 +0200

    feat: RFOX.sol

--- src/RFOX.sol
+++ src/RFOX.sol
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
+// TODO(gomes): import from OpenZeppelin
 interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
